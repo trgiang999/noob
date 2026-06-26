9fc6fb66368a6ff557c26faf16fa60983f8c3f5f
trgiang999 <trgiang123123123123@gmail.com>
2026-06-26 19:44:37 +0700

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,44 @@
+if not game:IsLoaded() then game.Loaded:Wait() end
+if getgenv().GHUB_LOADED then return end
+
+type ExecuteMap = {[number]: string}
+type ChapterMap = {[string]: number}
+
+local ChapterTable: ChapterMap = {
+    --Book 1
+    ['chapter1'] = 14787381917,
+    ['chapter2'] = 15322497988,
+    ['chapter3.1'] = 16375066410,
+    ['chapter3.2'] = 16485242214,
+    ['chapter3.3'] = 16554037885,
+    ['chapter4.1'] = 17619037026,
+    ['chapter4.2'] = 17680488855,
+    --Book 2
+    ['chapter1_b2'] = 71718624482170
+
+}
+
+local LoaderTable: ExecuteMap = {
+    [ChapterTable['chapter1']] = "https://raw.githubusercontent.com/trgiang999/noob/refs/heads/main/wsd_chapter1.lua",
+    [ChapterTable['chapter2']] = "https://raw.githubusercontent.com/trgiang999/noob/refs/heads/main/wsd_chapter2.lua",
+    [ChapterTable['chapter3.1']] = 'https://raw.githubusercontent.com/trgiang999/noob/refs/heads/main/wsd_chapter3.1.lua',
+    [ChapterTable['chapter3.2']] = '',
+    [ChapterTable['chapter3.3']] = '',
+    [ChapterTable['chapter4.1']] = '',
+    [ChapterTable['chapter4.2']] = '',
+    [ChapterTable['chapter1_b2']] = ''
+}
+
+local function githubGetRaw(direct_url)
+    local rawUrl = direct_url .. '?t=' .. tostring(os.time())
+    local success, content = pcall(game.HttpGetAsync, game, rawUrl)
+    if not success or content == "404: Not Found" then
+        print('UNSUPPORTED')
+        return ''
+    end
+    return content
+end
+
+url = LoaderTable[game.PlaceId]  --không có placeid/attempt placeid khác thì ko chạy luôn.
+raw = githubGetRaw(url)
+loadstring(raw)()
